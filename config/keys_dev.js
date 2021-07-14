7a2f867bc849f13599539c4abdefa6d6ad0d8b61
ananthjos <68706680+ananthjos@users.noreply.github.com>
2021-07-14 19:23:05 +0530

    updated config

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector',
+  mongoURI:
+    "mongodb+srv://ananth:pappa%23$508@cluster0.perbn.mongodb.net/estore?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
